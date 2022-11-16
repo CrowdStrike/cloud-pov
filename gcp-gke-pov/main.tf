dae8dd85f8404fc282d8bbff71ebf8a8a54ce07a
Ryan Payne <ryanj.payne@outlook.com>
2022-11-16 14:36:42 -0600

    add autopilot option

--- gcp-gke-pov/main.tf
+++ gcp-gke-pov/main.tf
@@ -17,8 +17,9 @@ module "gke" {
     
     gke_num_nodes = var.gke_num_nodes
     node_os = var.node_os
-    vpc_name  = module.network.network_name
-    subnet_name   = module.network.subnet_1_name
+    vpc_name = module.network.network_name
+    subnet_name = module.network.subnet_1_name
+    autopilot = var.autopilot
 }
 
 module "falcon" {
@@ -50,4 +51,5 @@ module "detection_container" {
 module "prometheus" {
     source = "github.com/CrowdStrike/terraform-modules/misc/prometheus"
     count = var.prometheus == true ? 1 : 0
+    cloud = "gcp"
 }
