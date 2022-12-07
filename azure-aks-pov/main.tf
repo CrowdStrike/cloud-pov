1f7b88e2c7eeb078aaeb5600ddbbd9550022e685
Ryan Payne <ryanj.payne@outlook.com>
2022-12-07 11:29:24 -0600

    add aks configuration options

--- azure-aks-pov/main.tf
+++ azure-aks-pov/main.tf
@@ -1,23 +1,28 @@
 
 module "aks" {
-    source = "github.com/CrowdStrike/terraform-modules/azure/aks"    
-    alias  = var.alias
-    location = var.location
-    resource_group = var.resource_group
-    cluster_name = var.cluster_name
-    aks_num_nodes = var.aks_num_nodes
+    source             = "github.com/CrowdStrike/terraform-modules/azure/aks"
+    alias              = var.alias
+    location           = var.location
+    resource_group     = var.resource_group
+    cluster_name       = var.cluster_name
+    kubernetes_version = var.kubernetes_version
+    aks_num_nodes      = var.aks_num_nodes
+    aks_min_nodes      = var.aks_min_nodes
+    aks_max_nodes      = var.aks_max_nodes
+    node_vm_size       = var.node_vm_size
+    os_sku             = var.os_sku
 }
 
 module "falcon" {
-    source = "github.com/CrowdStrike/terraform-modules/falcon/operator"
-    client_id = var.client_id
+    source        = "github.com/CrowdStrike/terraform-modules/falcon/operator"
+    client_id     = var.client_id
     client_secret = var.client_secret
-    sensor_type = var.sensor_type
-    environment = var.alias
+    sensor_type   = var.sensor_type
+    environment   = var.alias
 }
 
 module "protection_agent" {
-    source = "github.com/CrowdStrike/terraform-modules/falcon/k8s-protection-agent"
+    source                  = "github.com/CrowdStrike/terraform-modules/falcon/k8s-protection-agent"
     protection_agent_config = <<EOF
 crowdstrikeConfig:
   clientID: ${var.client_id}
@@ -31,11 +36,11 @@ EOF
 
 module "detection_container" {
     source = "github.com/CrowdStrike/terraform-modules/falcon/detection-container"
-    count = var.detection_container == true ? 1 : 0
+    count  = var.detection_container == true ? 1 : 0
 }
     
 module "prometheus" {
-    source = "github.com/CrowdStrike/terraform-modules/misc/prometheus"
-    count = var.prometheus == true ? 1 : 0
-    cloud = "gcp"
+    source = "github.com/CrowdStrike/terraform-modules/misc/prometheus"    
+    count  = var.prometheus == true ? 1 : 0
+    cloud  = "gcp"
 }
